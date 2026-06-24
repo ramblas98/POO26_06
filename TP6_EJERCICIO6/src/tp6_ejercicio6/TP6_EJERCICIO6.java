ad27383332d371e9c725e5931154421e127b6df3
Pc <Pc@DESKTOP-G99RR9M>
2026-06-24 17:17:17 -0300

    prueba

---
+++ TP6_EJERCICIO6/src/tp6_ejercicio6/TP6_EJERCICIO6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp6_ejercicio6;
+
+/**
+ *
+ * @author Pc
+ */
+public class TP6_EJERCICIO6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
